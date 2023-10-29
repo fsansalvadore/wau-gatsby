0aef029dc52e135b19eab8071c3fbba1a1264bce
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-10-29 16:23:21 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `wau-gatsby`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "https://wp.wauarchitetti.com/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
