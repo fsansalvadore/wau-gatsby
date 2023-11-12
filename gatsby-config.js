3dc2478d2308a140efddb713f605e1dc8ea76fa4
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-11-12 18:47:28 +0100

    clean up gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,9 +13,6 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         url: 'https://wp.wauarchitetti.com/graphql',
-        // typeName: 'WPGraphQL',
-        // fieldName: 'wpgraphql',
-        // useACF: true,
       },
     },
     'gatsby-plugin-image',
@@ -23,12 +20,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-postcss',
     'gatsby-plugin-sass',
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        // Add any options here
-      },
-    },
+    'gatsby-plugin-styled-components',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -54,45 +46,17 @@ module.exports = {
         trackingId: 'UA-195952687-1',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
-        // Setting this parameter is optional
-        // anonymize: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        // pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // // Enables Google Optimize Experiment ID
-        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // // Set Variation ID. 0 for original 1,2,3....
-        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // // Defers execution of google analytics script after page load
         defer: false,
-        // Any additional optional fields
-        // sampleRate: 5,
-        // siteSpeedSampleRate: 10,
-        // cookieDomain: "https://www.wauarchitetti.com/",
       },
     },
     {
       resolve: 'gatsby-plugin-gdpr-cookies',
       options: {
         googleAnalytics: {
-          trackingId: 'UA-195952687-1', // leave empty if you want to disable the tracker
-          cookieName: 'wau', // default
-          anonymize: true, // default
+          trackingId: 'UA-195952687-1',
+          cookieName: 'wau',
+          anonymize: true,
         },
-        // googleTagManager: {
-        //   trackingId: 'YOUR_GOOGLE_TAG_MANAGER_TRACKING_ID', // leave empty if you want to disable the tracker
-        //   cookieName: 'gatsby-gdpr-google-tagmanager', // default
-        //   dataLayerName: 'dataLayer', // default
-        // },
-        // facebookPixel: {
-        //   pixelId: 'YOUR_FACEBOOK_PIXEL_ID', // leave empty if you want to disable the tracker
-        //   cookieName: 'gatsby-gdpr-facebook-pixel', // default
-        // },
         // defines the environments where the tracking should be available  - default is ["production"]
         environments: ['production', 'development'],
       },
@@ -105,8 +69,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#000000`,
-        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
-        // crossOrigin: `use-credentials`,
+        icon: `src/images/favicon.svg`,
       },
     },
   ],
